35500ff247f12dd07bcb824fca52d6994ccc3d7a
choerul Umam <choerul Umam>
2017-03-20 19:51:49 +0700

    Under Development

--- logout.php
+++ logout.php
@@ -1,7 +1,14 @@
 <?php
+include ("db_connect.php");
 session_start();
 if(session_destroy()) // Destroying All Sessions
 {
-	header("Location: index.php"); // Redirecting To Home Page
+    $query_temp_delete = mysql_query("DELETE FROM temp_host", $connection);
+    if ($query_temp_delete){
+        header("Location: index.php"); // Redirecting To Home Page
+        mysql_close($connection);
+    } else {
+        echo "Error connection";
+    }
 }
 ?>
